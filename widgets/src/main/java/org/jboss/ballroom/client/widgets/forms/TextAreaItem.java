a850f13d0041ffbdde8bfdf231161f5ab8832322
David Bosschaert <david.bosschaert@gmail.com>
2011-08-01 16:04:58 +0100

    New TextAreaItem and unit test
    Also added unit testing support in Ballroom. The unit tests are fired up in
    the ordinary test phase.

---
+++ widgets/src/main/java/org/jboss/ballroom/client/widgets/forms/TextAreaItem.java
@@ -0,0 +1,93 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.jboss.ballroom.client.widgets.forms;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author Heiko Braun
+ * @author David Bosschaert
+ */
+public class TextAreaItem extends FormItem<String> {
+    protected TextArea textArea;
+    private InputElementWrapper wrapper;
+    ValueChangeHandler<String> valueChangeHandler;
+
+    public TextAreaItem(String name, String title) {
+        super(name, title);
+
+        textArea = new TextArea();
+        textArea.setName(name);
+        textArea.setTitle(title);
+        valueChangeHandler = new ValueChangeHandler<String>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<String> event) {
+                isModified = true;
+            }
+        };
+        textArea.addValueChangeHandler(valueChangeHandler);
+        wrapper = new InputElementWrapper(textArea, this);
+    }
+
+    @Override
+    public Widget asWidget() {
+        return wrapper;
+    }
+
+    @Override
+    public String getValue() {
+        return textArea.getValue();
+    }
+
+    @Override
+    protected void resetMetaData() {
+        super.resetMetaData();
+        textArea.setValue(null);
+    }
+
+    @Override
+    public void setValue(String value) {
+        textArea.setValue(value);
+    }
+
+
+    @Override
+    public void setEnabled(boolean b) {
+        textArea.setEnabled(b);
+    }
+
+    @Override
+    public void setErroneous(boolean b) {
+        super.setErroneous(b);
+        wrapper.setErroneous(b);
+    }
+
+    @Override
+    public boolean validate(String value) {
+        return !(isRequired() && value.trim().equals(""));
+    }
+
+    @Override
+    public void clearValue() {
+        textArea.setText("");
+    }
+}
---
+++ widgets/src/test/java/org/jboss/ballroom/client/widgets/forms/TextAreaItemTest.java
@@ -0,0 +1,94 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.jboss.ballroom.client.widgets.forms;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * @author David Bosschaert
+ */
+public class TextAreaItemTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "org.jboss.ballroom.Framework";
+    }
+
+    @Test
+    public void testTextAreaItem() {
+        TextAreaItem tai = new TextAreaItem("a", "b");
+        Assert.assertEquals("a", tai.getName());
+        Assert.assertEquals("b", tai.getTitle());
+
+        tai.textArea.setText("ho\nho");
+        Assert.assertEquals("ho\nho", tai.getValue());
+
+        tai.setValue("hi");
+        Assert.assertEquals("hi", tai.getValue());
+
+        tai.clearValue();
+        Assert.assertEquals("", tai.getValue());
+
+        tai.setEnabled(false);
+        Assert.assertFalse(tai.textArea.isEnabled());
+        tai.setEnabled(true);
+        Assert.assertTrue(tai.textArea.isEnabled());
+    }
+
+    @Test
+    public void testErroneous() {
+        TextAreaItem tai = new TextAreaItem("a", "b");
+        Assert.assertFalse("Precondition", tai.isErroneous);
+        tai.setErroneous(true);
+        Assert.assertTrue(tai.isErroneous);
+        tai.resetMetaData();
+        Assert.assertFalse(tai.isErroneous);
+    }
+
+    @Test
+    public void testModification() {
+        TextAreaItem tai = new TextAreaItem("a", "b");
+        Assert.assertFalse("Precondition", tai.isModified());
+        tai.valueChangeHandler.onValueChange(null);
+        Assert.assertTrue(tai.isModified());
+        tai.resetMetaData();
+        Assert.assertFalse(tai.isModified());
+    }
+
+    @Test
+    public void testValidate1() {
+        TextAreaItem tai = new TextAreaItem("a", "b");
+        tai.setRequired(true);
+        Assert.assertFalse(tai.validate(""));
+        Assert.assertTrue(tai.validate("hi ho"));
+        Assert.assertTrue(tai.validate("hi\nho"));
+    }
+
+    @Test
+    public void testValidate2() {
+        TextAreaItem tai = new TextAreaItem("a", "b");
+        tai.setRequired(false);
+        Assert.assertTrue(tai.validate(""));
+        Assert.assertTrue(tai.validate("hi ho"));
+        Assert.assertTrue(tai.validate("hi\nho"));
+    }
+}
