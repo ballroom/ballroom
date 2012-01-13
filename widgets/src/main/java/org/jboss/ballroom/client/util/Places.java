9dd5382a47b856a2894f7471e043f645e940183b
Heiko Braun <ike.braun@googlemail.com>
2012-01-13 15:43:44 +0100

    simplify LHSNavTRee

--- widgets/src/main/java/org/jboss/ballroom/client/util/Places.java
+++ widgets/src/main/java/org/jboss/ballroom/client/util/Places.java
@@ -30,6 +30,7 @@ import java.util.List;
  */
 public class Places {
 
+    @Deprecated
     public static List<PlaceRequest> fromString(String urlString)
     {
         List<PlaceRequest> places = null;
