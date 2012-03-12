bdb3dff1bdc912f47a0eec1d35a577ec5bed92d7
Heiko Braun <ike.braun@googlemail.com>
2012-03-12 14:40:38 +0100

    the trapped focus panel implementation

---
+++ widgets/src/main/java/org/jboss/ballroom/client/widgets/window/TrappedFocusPanel.java
@@ -0,0 +1,85 @@
+package org.jboss.ballroom.client.widgets.window;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * A panel that trap focus on it's child contents.
+ *
+ * @see Focus
+ *
+ * @author Heiko Braun
+ * @date 3/12/12
+ */
+public class TrappedFocusPanel extends LayoutPanel {
+
+    protected Focus focus;
+
+    public TrappedFocusPanel() {
+        super();
+        setStyleName("fill-layout");
+        this.sinkEvents(Event.ONKEYDOWN);
+
+        focus = new Focus(getElement());
+
+    }
+
+    public TrappedFocusPanel(Widget child) {
+        super();
+        setStyleName("fill-layout");
+        this.sinkEvents(Event.ONKEYDOWN);
+        add(child);
+
+        focus = new Focus(getElement());
+
+    }
+
+    @Override
+    protected void onAttach() {
+        super.onAttach();
+        Scheduler.get().scheduleDeferred(
+                new Scheduler.ScheduledCommand() {
+                    @Override
+                    public void execute() {
+                        //focus = new Focus(getElement());
+                        focus.setDefault();
+                    }
+                }
+        );
+
+    }
+
+    public Focus getFocus() {
+        return focus;
+    }
+
+    @Override
+    public void onBrowserEvent(Event event) {
+
+        int type = DOM.eventGetType(event);
+        switch (type) {
+            case Event.ONKEYDOWN:
+                if(event.getKeyCode()== KeyCodes.KEY_TAB)
+                {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    if(event.getShiftKey())
+                        focus.prev();
+                    else
+                        focus.next();
+
+                }
+                break;
+            default:
+                return;
+
+        }
+
+    }
+}
+
