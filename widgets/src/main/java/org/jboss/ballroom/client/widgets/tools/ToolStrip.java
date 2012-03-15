e62322db2ae1ac542852bf8d0421a8392a00c608
Heiko Braun <ike.braun@googlemail.com>
2012-03-15 09:51:22 +0100

    evert toolstrip changes: no custom kbd navigation. tabindex is sufficient

--- widgets/src/main/java/org/jboss/ballroom/client/widgets/tools/ToolStrip.java
+++ widgets/src/main/java/org/jboss/ballroom/client/widgets/tools/ToolStrip.java
@@ -19,122 +19,37 @@
 
 package org.jboss.ballroom.client.widgets.tools;
 
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import org.jboss.ballroom.client.widgets.forms.ComboBox;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * @author Heiko Braun
  * @date 2/28/11
  */
-public class ToolStrip extends FocusPanel {
+public class ToolStrip extends HorizontalPanel{
 
     private HorizontalPanel left;
     private HorizontalPanel right;
 
-    private HorizontalPanel layout;
-    private int currBtnIdx = -1;
-    private LinkedList<ToolButton> buttons = new LinkedList<ToolButton>();
-
     public ToolStrip() {
         super();
-
-        setTabIndex(-1);
-
-        layout = new HorizontalPanel();
-        layout.setStyleName("default-toolstrip");
+        setStyleName("default-toolstrip");
 
         left = new HorizontalPanel();
         right = new HorizontalPanel();
 
-        layout.add(left);
-        layout.add(right);
+        add(left);
+        add(right);
 
         left.getElement().getParentElement().setAttribute("width", "50%");
         right.getElement().getParentElement().setAttribute("width", "50%");
         right.getElement().getParentElement().setAttribute("align", "right");
-
-        add(layout);
-
-        addKeyDownHandler(new KeyDownHandler() {
-            @Override
-            public void onKeyDown(KeyDownEvent event) {
-                if(event.getNativeKeyCode()== KeyCodes.KEY_DOWN)
-                {
-                    nextButton();
-                    event.stopPropagation();
-                }
-                else if(event.getNativeKeyCode()== KeyCodes.KEY_UP)
-                {
-                    prevButton();
-                    event.stopPropagation();
-                }
-            }
-        }) ;
-    }
-
-    private void prevButton() {
-        if(buttons.isEmpty()) return;
-
-        // remove focus on current selection
-        if(currBtnIdx>=0 && currBtnIdx<buttons.size())
-            blur(buttons.get(currBtnIdx));
-
-        // focus on next button
-        int prev = currBtnIdx-1;
-
-        if(prev<0)
-            prev=buttons.size()-1;
-
-        currBtnIdx=prev;
-
-        focus(buttons.get(prev));
-
-    }
-
-    private void nextButton() {
-
-        if(buttons.isEmpty()) return;
-
-        // remove focus on current selection
-        if(currBtnIdx>=0 && currBtnIdx<buttons.size())
-            blur(buttons.get(currBtnIdx));
-
-        // focus on next button
-        int next = currBtnIdx+1;
-
-        if(next >= buttons.size())
-            next=0;
-
-        currBtnIdx=next;
-
-        focus(buttons.get(next));
-
-    }
-
-    private void blur(Widget widget) {
-        widget.getElement().blur();
-        widget.getElement().setAttribute("tabindex", "-1");
-    }
-
-    private void focus(Widget widget) {
-        widget.getElement().focus();
-        widget.getElement().setAttribute("tabindex", "0");
     }
 
     public void addToolButton(ToolButton button)
     {
         left.add(button);
-        buttons.add(button);
     }
 
     public void addToolButtonRight(ToolButton button)
@@ -142,7 +57,7 @@ public class ToolStrip extends FocusPanel {
         button.getElement().setAttribute("style", "margin-right:5px;");
         button.addStyleName("toolstrip-button-secondary");
         right.add(button);
-        buttons.add(button);
+
     }
 
     public boolean hasButtons() {
