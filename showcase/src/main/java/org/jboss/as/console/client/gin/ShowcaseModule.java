dc02e7dd59ab0a564e3263916e581ef78d4b3224
Harald Pehl <harald.pehl@gmail.com>
2013-04-04 17:10:13 +0200

    Switch to Java7, GWT 2.5.1, GIN 2.0.0, GWTP 1.0-RC-2

--- showcase/src/main/java/org/jboss/as/console/client/gin/ShowcaseModule.java
+++ showcase/src/main/java/org/jboss/as/console/client/gin/ShowcaseModule.java
@@ -1,14 +1,12 @@
 package org.jboss.as.console.client.gin;
 
-import com.google.web.bindery.event.shared.EventBus;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.inject.Singleton;
-import com.gwtplatform.mvp.client.DefaultProxyFailureHandler;
+import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.RootPresenter;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.proxy.ParameterTokenFormatter;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
-import com.gwtplatform.mvp.client.proxy.ProxyFailureHandler;
 import com.gwtplatform.mvp.client.proxy.TokenFormatter;
 import org.jboss.as.console.client.DefaultPlaceManager;
 import org.jboss.as.console.client.WidgetPresenter;
@@ -24,7 +22,6 @@ public class ShowcaseModule extends AbstractPresenterModule {
         bind(PlaceManager.class).to(DefaultPlaceManager.class).in(Singleton.class);
         bind(TokenFormatter.class).to(ParameterTokenFormatter.class).in(Singleton.class);
         bind(RootPresenter.class).asEagerSingleton();
-        bind(ProxyFailureHandler.class).to(DefaultProxyFailureHandler.class).in(Singleton.class);
 
         // main layout
         bindPresenter(MainLayoutPresenter.class,
